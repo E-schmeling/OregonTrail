cd26a8371e9b2b4196ea30c8177247cdfbc1961d
Austin Hanni-Wells <a-hanni-wells@onu.edu>
2023-03-28 09:57:33 -0400

    Added to RandomNumber method to get sicknesses.

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -48,7 +48,7 @@ public class Map {
             daynumber++;
         }
 
-        public void RandomNumber(int health) {
+        public void RandomNumber(String name, int health) {
             int randNum = (int) (Math.random() % 1000);
             if (randNum >= 0 && randNum <= 6) { // snakebite occurs(0.7%), call event class
             }
