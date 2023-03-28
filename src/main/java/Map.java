68680557e60fbdb97413c137714a6ca13929726a
Austin Hanni-Wells <a-hanni-wells@onu.edu>
2023-03-28 10:03:53 -0400

    Split RandomEvent and RandomSickness into two separate methods.

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -5,15 +5,15 @@ import static java.lang.System.out;
 
 //I think we should probably have a map class
 public class Map {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //series of locations for testing
         ArrayList<Location> runnerLocations = new ArrayList<>();
 
         //Insert location adding loop at some point when aaron is Good
-        runnerLocations.add(new Location(0,"location0"));
-        runnerLocations.add(new Location(1000,"Location1"));
-        runnerLocations.add(new Location(2000,"location2"));
+        runnerLocations.add(new Location(0, "location0"));
+        runnerLocations.add(new Location(1000, "Location1"));
+        runnerLocations.add(new Location(2000, "location2"));
         runnerLocations.add(new Location(2500, "Special event crossing river"));//special event
 
         Map trail = new Map(runnerLocations);
@@ -25,45 +25,48 @@ public class Map {
     }
 
 
-        private ArrayList<Location> locations;
-        private int playerdistance=0;
-        private int daynumber = 0;//to deal with weather events, possibly add days depending on what month is picked and then subtract that at the end when displaying how long of a trip you had.
+    private ArrayList<Location> locations;
+    private int playerdistance = 0;
+    private int daynumber = 0;//to deal with weather events, possibly add days depending on what month is picked and then subtract that at the end when displaying how long of a trip you had.
 
 
+    public Map(ArrayList<Location> locations) {
+        this.locations = locations;
+    }
 
-        public Map(ArrayList<Location> locations) {
-            this.locations = locations;
-        }
-
-        public Location closestloc() {
-            for (Location loc : locations) {
-                if (loc.getMilesinfromstart() >= playerdistance) {
-                    return loc;
-                }
-            }
-            return locations.get(locations.size()-1);
-        }
-        public void AdvanceDay() {
-            playerdistance += 20;
-            daynumber++;
-        }
-
-        public void RandomNumber(String name, int health) {
-            int randNum = (int) (Math.random() % 1000);
-            if (randNum >= 0 && randNum <= 6) { // snakebite occurs(0.7%), call event class
-            }
-            else if (randNum >= 7 && randNum <= 26) { // lose trail(2.0%), call event class
-            }
-            else if (randNum >= 27 && randNum <= 46) { // thief comes during night(2.0%), call event class
-            }
-
-            int randSickness = (int) (Math.random() % 100);
-            if (health != 40) {
-                if (randSickness >= 0 && randSickness <= (40 - health)) {
-                    // one of the random sicknesses is given to this person
-                }
+    public Location closestloc() {
+        for (Location loc : locations) {
+            if (loc.getMilesinfromstart() >= playerdistance) {
+                return loc;
             }
         }
+        return locations.get(locations.size() - 1);
+    }
+
+    public void AdvanceDay() {
+        playerdistance += 20;
+        daynumber++;
+    }
+
+    public void RandomEvent(int health) {
+        int randNum = (int) (Math.random() % 1000);
+        if (randNum >= 0 && randNum <= 6) { // snakebite occurs(0.7%), call event class
+        }
+        else if (randNum >= 7 && randNum <= 26) { // lose trail(2.0%), call event class
+        }
+        else if (randNum >= 27 && randNum <= 46) { // thief comes during night(2.0%), call event class
+        }
+    }
+
+    public void RandomSickness(String name, int health) {
+        int randSickness = (int) (Math.random() % 100);
+        if (health != 40) {
+            if (randSickness >= 0 && randSickness <= (40 - health)) {
+                // one of the random sicknesses is given to this person
+            }
+        }
+    }
+
 
     @Override
     public String toString() {
@@ -73,7 +76,6 @@ public class Map {
                 ", daynumber=" + daynumber +
                 '}';
     }
-
 
 
 }
